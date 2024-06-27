4ef4dd035e50e59f5f7566d177ca44b654fd4e21
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-06-28 03:03:57 +0800

    minmax feel by separating scrollSpeed and logIntensity

--- src/boomer.nim
+++ src/boomer.nim
@@ -448,20 +448,22 @@ proc main() =
     while XPending(display) > 0:
       discard XNextEvent(display, addr xev)
 
-      proc scrollUp() =
+      proc scrollDown() =
         if (xev.xkey.state and ControlMask) > 0.uint32 and flashlight.isEnabled:
           flashlight.deltaRadius += INITIAL_FL_DELTA_RADIUS
         else:
-          let adjustedScale = log10(camera.scale + 1) / log10(config.scrollSpeed)
-          camera.deltaScale += adjustedScale
+          let logIntensity = 1.01
+          let adjustedScale = log10((camera.scale + 1) * logIntensity) / log10(logIntensity)
+          camera.deltaScale += adjustedScale * config.scrollSpeed
           camera.scalePivot = mouse.curr
 
-      proc scrollDown() =
+      proc scrollUp() =
         if (xev.xkey.state and ControlMask) > 0.uint32 and flashlight.isEnabled:
           flashlight.deltaRadius -= INITIAL_FL_DELTA_RADIUS
         else:
-          let adjustedScale = log10(camera.scale + 1) / log10(config.scrollSpeed)
-          camera.deltaScale -= adjustedScale
+          let logIntensity = 1.01
+          let adjustedScale = log10((camera.scale + 1) * logIntensity) / log10(logIntensity)
+          camera.deltaScale -= adjustedScale * config.scrollSpeed
           camera.scalePivot = mouse.curr
 
       case xev.theType
